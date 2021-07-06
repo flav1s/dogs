7f56fef60805cf75d4373594809f4661a642e838
Flávia Rocha <flaviaccruzr@gmail.com>
2021-07-06 08:30:43 -0300

    feat: introduction to dogs api

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import React from "react";
+import Api from "./api/Api";
+
+import "./App.css";
+
 function App() {
-  return <div className="App">App React</div>;
+  return (
+    <div>
+      <Api />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/api/Api.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+import UserPost from "./endpoints/UserPost";
+import TokenPost from "./endpoints/TokenPost";
+import PhotoPost from "./endpoints/PhotoPost";
+import PhotoGet from "./endpoints/PhotoGet";
+
+const Api = () => {
+  return (
+    <div>
+      <h1>User Post</h1>
+      <UserPost />
+      <h1>Token Post</h1>
+      <TokenPost />
+      <h1>Photo Post</h1>
+      <PhotoPost />
+      <h1>Photo Get</h1>
+      <PhotoGet />
+    </div>
+  );
+};
+
+export default Api;
