e4594f16da8c574024bbaf6ca394d9196a349c3d
Flávia Rocha <flaviaccruzr@gmail.com>
2021-07-08 08:37:12 -0300

    WIP: create initial structure

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React from "react";
-import Api from "./api/Api";
-
 import "./App.css";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import Home from "./pages/Home/Home";
+import Login from "./pages/Login/Login";
 
 function App() {
   return (
     <div>
-      <Api />
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
