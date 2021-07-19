8aa535946aee69fda645ef69c9d06027908b3737
Flávia Rocha <flaviaccruzr@gmail.com>
2021-07-19 07:54:53 -0300

    feat: create user context

--- src/pages/Login/LoginForm.js
+++ src/pages/Login/LoginForm.js
@@ -1,42 +1,20 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useContext } from "react";
 import { Link } from "react-router-dom";
-import { TOKEN_POST, USER_GET } from "../../api/api";
 import Button from "../../components/Form/Button";
 
 import Input from "../../components/Form/Input";
 import useForm from "../../hooks/useForm";
+import { UserContext } from "../../UserContext";
 
 const LoginForm = () => {
   const username = useForm();
   const password = useForm();
-
-  useEffect(() => {
-    const token = window.localStorage.getItem("token");
-    if (token) {
-      getUser(token);
-    }
-  }, []);
-
-  const getUser = async (token) => {
-    const { url, options } = USER_GET(token);
-    const response = await fetch(url, options);
-    const json = await response.json();
-    console.log(json);
-  };
+  const { userLogin } = useContext(UserContext);
 
   const handleSubmit = async (event) => {
     event.preventDefault();
     if (username.validate() && password.validate()) {
-      const { url, options } = TOKEN_POST({
-        username: username.value,
-        password: password.value,
-      });
-
-      const response = await fetch(url, options);
-      const json = await response.json();
-
-      window.localStorage.setItem("token", json.token);
-      getUser(json.token);
+      userLogin(username.value, password.value);
     }
   };
 
