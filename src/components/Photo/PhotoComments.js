787dc0540a2f6297ec575b7fda3c5b78512dc5e3
Flávia Rocha <flaviaccruzr@gmail.com>
2021-08-31 09:00:05 -0300

    feat: create comment section

--- src/components/Photo/PhotoComments.js
+++ src/components/Photo/PhotoComments.js
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useContext, useEffect, useRef, useState } from "react";
 
-const PhotoComments = () => {
-  return <div></div>;
+import { UserContext } from "../../UserContext";
+import PhotoCommentsForm from "./PhotoCommentsForm";
+
+import styles from "./PhotoComments.module.css";
+
+const PhotoComments = (props) => {
+  const [comments, setComments] = useState(() => props.comments);
+  const commentSection = useRef(null);
+
+  const { login } = useContext(UserContext);
+
+  useEffect(() => {
+    commentSection.current.scrollTop = commentSection.current.scrollHeight;
+  }, [comments]);
+
+  return (
+    <>
+      <ul ref={commentSection} className={styles.comments}>
+        {comments.map((comment) => (
+          <li key={comment.comment_ID}>
+            <b>{comment.comment_author}: </b>
+            <span>{comment.comment_content}</span>
+          </li>
+        ))}
+      </ul>
+      {login && <PhotoCommentsForm id={props.id} setComments={setComments} />}
+    </>
+  );
 };
 
 export default PhotoComments;
