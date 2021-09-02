d5bdfb815f5ca789e780ccad1447099e58cfe361
Flávia Rocha <flaviaccruzr@gmail.com>
2021-09-02 08:56:06 -0300

    feat: create img skeleton

--- src/components/Feed/FeedPhotosItem.js
+++ src/components/Feed/FeedPhotosItem.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Image from "../Helper/Image";
 
 import styles from "./FeedPhotosItem.module.css";
 
@@ -9,7 +10,7 @@ const FeedPhotosItem = ({ photo, setModalPhoto }) => {
 
   return (
     <li className={styles.photo} onClick={handleClickPhotos}>
-      <img src={photo.src} alt={photo.title} />
+      <Image src={photo.src} alt={photo.title} />
       <span className={styles.visualization}>{photo.acessos}</span>
     </li>
   );
