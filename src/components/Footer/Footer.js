8a89b7e3980c838702bacac4e0124e3f3c907e43
Flávia Rocha <flaviaccruzr@gmail.com>
2021-09-13 09:21:36 -0300

    feat: create footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,8 +1,14 @@
 import React from "react";
+import { ReactComponent as Dogs } from "../../assets/dogs-footer.svg";
 import styles from "./Footer.module.css";
 
 const Footer = () => {
-  return <div className={styles.footer}>Footer</div>;
+  return (
+    <div className={styles.footer}>
+      <Dogs />
+      <p>Dogs. Alguns direitos reservados</p>
+    </div>
+  );
 };
 
 export default Footer;
