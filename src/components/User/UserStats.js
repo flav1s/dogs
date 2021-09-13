5673178753e9a31fa6ee16aabbbd0c4fa5742056
Flávia Rocha <flaviaccruzr@gmail.com>
2021-09-13 11:31:48 -0300

    feat: create stats page

--- src/components/User/UserStats.js
+++ src/components/User/UserStats.js
@@ -1,12 +1,33 @@
-import React from "react";
+import React, { lazy, useEffect } from "react";
 import Head from "../Helper/Head";
+import useFetch from "../../hooks/useFetch";
+import { STATS_GET } from "../../api/api";
+
+import Loading from "../../components/Loading/Loading";
+import { Error } from "../../components/Error/Error";
+
+const UserStatsGraphs = lazy(() => import("./UserStatsGraphs"));
 
 const UserStats = () => {
+  const { data, error, request, loading } = useFetch();
+
+  useEffect(() => {
+    async function getData() {
+      const { url, options } = STATS_GET();
+      await request(url, options);
+    }
+    getData();
+  }, [request]);
+
+  if (loading) return <Loading />;
+  if (error) return <Error error={error} />;
+  if (!data) return null;
+
   return (
-    <div>
-      {" "}
+    <React.Suspense fallback={<div />}>
       <Head title="Estatísticas" />
-    </div>
+      <UserStatsGraphs data={data} />
+    </React.Suspense>
   );
 };
 
